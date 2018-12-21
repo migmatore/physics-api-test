3980b1cb61e085f1e85dc44faa8383a86e2e3f8d
moz200 <kazakvova201@gmail.com>
2018-12-21 12:45:06 +0300

    add MainApiController(/api/plus /api/minus)

--- src/main/kotlin/com/esaiscorp/physicsapi/controller/MainApiController.kt
+++ src/main/kotlin/com/esaiscorp/physicsapi/controller/MainApiController.kt
@@ -2,14 +2,46 @@ package com.esaiscorp.physicsapi.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
 class MainApiController {
+    abstract class Math {
+        abstract var result: Int
+    }
 
     @GetMapping
     fun index(): String {
         return "This is API"
+    }
+
+    @GetMapping("/plus")
+    fun plus(
+            @RequestParam("num1") num1: Int,
+            @RequestParam("num2") num2: Int
+    ): Any {
+        data class Plus(
+                override var result: Int
+        ) : Math()
+
+        return Plus(
+                result = num1 + num2
+        )
+    }
+
+    @GetMapping("/minus")
+    fun minus(
+            @RequestParam("num1") num1: Int,
+            @RequestParam("num2") num2: Int
+    ): Any {
+        data class Minus(
+                override var result: Int
+        ) : Math()
+
+        return Minus(
+                result = num1 - num2
+        )
     }
 }
