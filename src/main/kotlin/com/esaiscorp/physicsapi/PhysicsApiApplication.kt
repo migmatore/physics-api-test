5f42c25bc04cf36d93cc020c1b7d7e41903a1658
moz200 <kazakvova201@gmail.com>
2018-12-21 12:21:56 +0300

    create project

---
+++ src/main/kotlin/com/esaiscorp/physicsapi/PhysicsApiApplication.kt
@@ -0,0 +1,12 @@
+package com.esaiscorp.physicsapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhysicsApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<PhysicsApiApplication>(*args)
+}
+
